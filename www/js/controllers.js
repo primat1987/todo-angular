6099b4045b43404fcb2d85f04f9c0ce2262aa0e4
Alex <sashamgu@rambler.ru>
2015-05-04 17:39:20 +0300

    new functionality - remove and edit todos.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,21 +4,51 @@
 
 var phonecatApp = angular.module('todoApp', []);
 
-phonecatApp.controller('todoController', ['$scope', function($scope) {
+phonecatApp.controller('todoController', ['$scope', '$filter', function($scope, $filter) {
   $scope.todos = [
       {
         'title': 'Build a to do app',
         'done':true
       }];
 
+  $scope.$watch('todos', function (newValue, oldValue) {
+      console.log(oldValue[oldValue.length-1]);
+      $scope.remainingCount = $filter('filter')($scope.todos, { 'done': false }).length;
+      //$scope.completedCount = todos.length - $scope.remainingCount;
+      //$scope.allChecked = !$scope.remainingCount;
+    }, true);
+
   $scope.addToDo = function(){
-    $scope.todos.push({'title': $scope.newtodo, 'done' : false});
+    //if($scope.newtodo)
+    var newItem = {
+      'title': $scope.newtodo,
+      'done': false
+    };
+
+    if(!newItem.title){
+      return;
+    }
+    
+    $scope.todos.push(newItem);
     $scope.newtodo = '';
+  };
+
+  $scope.removeToDo = function(todo){
+    var index = $scope.todos.indexOf(todo);
+    if(index > -1) {
+      $scope.todos.splice(index, 1);
+    };
   };
 
   $scope.clearCompleted = function(){
     $scope.todos = $scope.todos.filter(function(item){
       return !item.done;
     })
+  };
+
+  $scope.editToDo = function(todo){
+    $scope.editedTodo = todo;
+    $scope.originalTodo = angular.extend({}, todo);
+    
   };
 }]);
